82b7b65d0e34e9508aca0a5eb83cdf3e74f67e62
ryan-deepin <a563645043@gmail.com>
2021-01-28 23:19:44 +0800

    http_update

---
+++ Python_automation/11_从web抓取信息/11.6_selenium/06_点击浏览器按钮.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chrome_drive = r"/home/ryan/Downloads/chromedriver"
+browser = webdriver.Chrome(executable_path=chrome_drive)
+browser.get(r"https://www.baidu.com")
+kw = browser.find_element_by_id('kw')
+kw.send_keys('test')
+su = browser.find_element_by_id('su')
+su.submit()
+time.sleep(1)
+html = browser.find_element_by_tag_name('html')
+html.send_keys(Keys.END)
+time.sleep(0.5)
+html.send_keys(Keys.HOME)
+time.sleep(0.5)
+browser.quit()  # quit为退出、forward为前进、back为返回、refresh为刷新
