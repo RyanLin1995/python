f40e79082832ecd497dd1ee6adc61cfbc9701e1c
RyanLin <63003303+RyanLin1995@users.noreply.github.com>
2022-04-24 11:02:57 +0800

    类对函数装饰更新

--- python_improve/06_mini-web框架/03_装饰器/11_用类对函数进行装饰.py
+++ python_improve/06_mini-web框架/03_装饰器/11_用类对函数进行装饰.py
@@ -1,10 +1,14 @@
+# 不带参数时的类装饰器
+# init ：接收被装饰函数
+# call ：实现装饰逻辑
+
 class Test(object):
 
-    def __init__(self, func):
+    def __init__(self, func):  # __init__ 方法接收被装饰的方法
 
         self.func = func
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwargs):  # __call__ 方法实现方法
 
         print("这是一个装饰器")
         print("开始装饰")
@@ -12,6 +16,35 @@ class Test(object):
 
 
 @Test
+def test():
+    print("hahahahah")
+
+
+test()
+
+
+# 带参数时的类装饰器
+# init ：不再接收被装饰函数，而是接收传入参数
+# call ：接收被装饰函数，实现装饰逻辑
+
+class Test(object):
+
+    def __init__(self, loglevel):
+
+        self.loglevel = loglevel
+
+    def __call__(self, func):  # __call__ 方法接收函数
+        def wrapper(*args, **kwargs):
+            if self.loglevel == 'debug':
+                print("这是一个装饰器")
+                print("开始装饰")
+                print(f'[DEBUG] enter {func.__name__}()')
+                func(*args, **kwargs)
+                print("装饰结束")
+        return wrapper
+
+
+@Test('debug')
 def test():
     print("hahahahah")
 
