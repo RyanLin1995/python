cb145dde47acf7eb910de00854cac6538c77a766
deepin-ryan <a563645043@gmail.com>
2022-05-22 14:50:01 +0800

    天天生鲜项目完结撒花

--- django/dailyfresh/dailyfresh/settings.py
+++ django/dailyfresh/dailyfresh/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3qi$5@9mxv55^u9c^=z*6^gz(qtjkfqi@ba#fzo+&=x3a@@yce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
